0be2ea958fc9a2f0ef49008fd9cc65c9d35be5df
marco238 <marco238cuba@gmail.com>
2020-06-25 22:48:25 +0200

    feat: basic views

--- flaskapp.py
+++ flaskapp.py
@@ -1,43 +1,79 @@
 from flask import Flask, escape, request, render_template
 app = Flask(__name__)
 
-posts = [
+purchases = [
     {
-        'author': 'Luis Ariel',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2020'
+        'from': 'USD',
+        'quantity_from': 100,
+        'to': 'CUC',
+        'quantity_to': 90,
+        'date': 'April 20, 2020 - 12:00',
+        'PU': 25
     },
     {
-        'author': 'Aaron Luis',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 23, 2020'
+        'from': 'EUR',
+        'quantity_from': 12,
+        'to': 'USD',
+        'quantity_to': 20,
+        'date': 'April 2, 2020 - 14:00',
+        'PU': 35
     },
     {
-        'author': 'Lis Serrano',
-        'title': 'Blog Post 3',
-        'content': 'Third post content',
-        'date_posted': 'April 3, 2020'
+        'from': 'MN',
+        'quantity_from': 1000,
+        'to': 'CUC',
+        'quantity_to': 40,
+        'date': 'April 5, 2020 - 16:00',
+        'PU': 45
     },
     {
-        'author': 'Chabelys Pérez',
-        'title': 'Blog Post 4',
-        'content': 'Fourth post content',
-        'date_posted': 'March 10, 2020'
+        'from': 'EUR',
+        'quantity_from': 1,
+        'to': 'MN',
+        'quantity_to': 24,
+        'date': 'April 10, 2020 - 18:00',
+        'PU': 60
+    },
+    {
+        'from': 'MN',
+        'quantity_from': 111,
+        'to': 'MN',
+        'quantity_to': 124,
+        'date': 'April 1, 2020 - 11:00',
+        'PU': 100
+    },
+    {
+        'from': 'USD',
+        'quantity_from': 900,
+        'to': 'EUR',
+        'quantity_to': 1000,
+        'date': 'April 30, 2020 - 8:00',
+        'PU': 50
     },
 ]
 
+#*********************************************#
+#*******************Router********************#
+#*********************************************#
 @app.route('/')
 @app.route('/home')
 def home():
-    name = request.args.get("name", "Home")
-    return render_template('./home_view/home.html', title = 'Home', posts = posts)
+    return render_template(
+        './home_view/home.html',
+        title = 'Home',
+        purchases = purchases
+    )
 
-@app.route('/about')
+@app.route('/purchase')
+def purchase():
+    return render_template('./purchase_view/purchase.html', title = 'Purchase')
+
+@app.route('/status')
 def about():
-    name = request.args.get("name", "About")
-    return render_template('./about_view/about.html', title = 'About')
+    return render_template('./status_view/status.html', title = 'Status')
+#*********************************************#
+#*********************************************#
+#*********************************************#
 
 if __name__ == '__main__':
     app.run(debug=True)
